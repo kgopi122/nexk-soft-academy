a0026b09a5403f69e8b9c664fef0899a2abc3f0e
kgopi122 <gsai4801@gmail.com>
2025-12-12 21:12:46 +0530

    feat: configure github pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,7 @@ export default defineConfig(({ mode }) => ({
     host: "::",
     port: 8080,
   },
+  base: "/nexk-soft-academy/",
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
     alias: {
