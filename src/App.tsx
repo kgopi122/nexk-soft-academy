9af575eff4353d411cbf959d72beda0c168042c3
kgopi122 <gsai4801@gmail.com>
2025-12-12 21:16:04 +0530

    fix: add basename to router for github pages

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/nexk-soft-academy">
         <Routes>
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
