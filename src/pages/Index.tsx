93db2ac48ee256866f4119df2b956eb2473298dd
kgopi122 <gsai4801@gmail.com>
2025-12-12 21:08:02 +0530

    first commit

---
+++ src/pages/Index.tsx
@@ -0,0 +1,27 @@
+import Navbar from "@/components/Navbar";
+import HeroSection from "@/components/HeroSection";
+import AboutSection from "@/components/AboutSection";
+import CourseSection from "@/components/CourseSection";
+import DemoSection from "@/components/DemoSection";
+import WhyUsSection from "@/components/WhyUsSection";
+import TestimonialsSection from "@/components/TestimonialsSection";
+import ContactSection from "@/components/ContactSection";
+import Footer from "@/components/Footer";
+
+const Index = () => {
+  return (
+    <div className="min-h-screen">
+      <Navbar />
+      <HeroSection />
+      <AboutSection />
+      <CourseSection />
+      <DemoSection />
+      <WhyUsSection />
+      <TestimonialsSection />
+      <ContactSection />
+      <Footer />
+    </div>
+  );
+};
+
+export default Index;
